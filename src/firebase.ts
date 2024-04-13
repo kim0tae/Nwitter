155485c53ff8ccd025743ff388a2e2a09f8fbeb6
Kim youngtae <kimyt0402@gmail.com>
2024-04-13 14:41:38 +0900

    [UPDATE] firebase 인증 추가
    email, password

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBzenlRt5fv-vjOx9u-N0fXgTym9IpsLrg",
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
