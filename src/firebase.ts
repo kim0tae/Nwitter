6952b87037f4005ab9c3ce181986b781854b1e55
Kim youngtae <kimyt0402@gmail.com>
2024-04-13 14:33:42 +0900

    [FEATURE] firebase 초기 설정

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzenlRt5fv-vjOx9u-N0fXgTym9IpsLrg",
+  authDomain: "nwitter-e10f0.firebaseapp.com",
+  projectId: "nwitter-e10f0",
+  storageBucket: "nwitter-e10f0.appspot.com",
+  messagingSenderId: "989325993058",
+  appId: "1:989325993058:web:4c2bf20785017ad58c9d3b",
+};
+
+const app = initializeApp(firebaseConfig);
