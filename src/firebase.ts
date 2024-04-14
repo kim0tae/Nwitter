fb46fd9f947ef246e0afc29065c9cbf49103f02e
Kim youngtae <kimyt0402@gmail.com>
2024-04-14 13:31:59 +0900

    [UPDATE] 트윗 작성 추가
    Firebase DB, Storage 추가

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBzenlRt5fv-vjOx9u-N0fXgTym9IpsLrg",
@@ -13,3 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
